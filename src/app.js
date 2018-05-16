995261ac828179e26f112bc6bc4f0d3367c2a865
Oli Evans <oli@tableflip.io>
2018-05-16 18:21:43 +0100

    fix: style experiments
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/app.js
+++ src/app.js
@@ -8,13 +8,22 @@ cytoscape.use(dagre)
 const {search} = window.location
 const params = search ? qs.parse(search.substring(1)) : {}
 const {path} = params
+const layoutOpts = {
+  name: 'dagre',
+  animate: true,
+  animationDuration: 300,
+  animationEasing: 'ease-in-out',
+  rankSep: 100,
+  nodeSep: 10
+}
 
 renderTree(window.ipfs, path)
 
-function addIpfsLinks (parent, links, cy) {
+function ipfsLinksToCy (parent, links) {
   if (!links.length) return
+  const showLinks = links // .slice(0, 6)
 
-  const edges = links.map(link => {
+  const edges = showLinks.map(link => {
     const obj = link.toJSON()
     const {multihash} = obj
     return {
@@ -27,7 +36,7 @@ function addIpfsLinks (parent, links, cy) {
     }
   })
 
-  const nodes = links.map(link => {
+  const nodes = showLinks.map(link => {
     const obj = link.toJSON()
     const {multihash, size, name} = obj
     return {
@@ -41,10 +50,12 @@ function addIpfsLinks (parent, links, cy) {
     }
   })
 
-  cy.add([...edges, ...nodes])
+  return [...edges, ...nodes]
 }
 
 async function renderTree (ipfs, path) {
+  const links = await ipfs.object.links(path)
+  const layer1 = ipfsLinksToCy(path, links)
   const cy = cytoscape({
     container: document.getElementById('ipld-graph'), // container to render in
 
@@ -55,46 +66,51 @@ async function renderTree (ipfs, path) {
           id: path,
           prettyHash: prettyHash(path)
         }
-      }
+      },
+      ...layer1
     ],
 
     style: [ // the stylesheet for the graph
       {
         selector: 'node',
         style: {
-          'background-color': '#666'
+          shape: 'roundrectangle',
+          width: '16px',
+          height: '16px',
+          'background-color': '#4A90E2'
         }
       },
 
       {
         selector: 'edge',
         style: {
-          'width': 1,
-          'line-color': '#ccc',
-          'target-arrow-color': '#ccc',
+          'curve-style': 'bezier',
+          'width': 2,
+          'line-color': '#bedbd6',
           'target-arrow-shape': 'triangle',
-          'target-label': 'data(prettyHash)',
-          'text-rotation': 'autorotate'
-
+          'target-arrow-color': '#bedbd6',
+          'label': 'data(prettyHash)',
+          'font-family': 'courier, avenir, sans-serif',
+          'font-size': '10px',
+          'text-rotation': 'autorotate',
+          'color': '#243a53'
         }
       }
     ],
 
-    layout: {
-      name: 'dagre'
-    }
+    layout: layoutOpts
   })
-
-  const links = await ipfs.object.links(path)
-  addIpfsLinks(path, links, cy)
-  cy.layout({ name: 'dagre' }).run()
 
   cy.on('tap', async (e) => {
     const data = e.target.data()
     const links = await ipfs.object.links(data.id)
-    addIpfsLinks(data.id, links, cy)
-    cy.layout({ name: 'dagre', animate: true, fit: false }).run()
+    cy.add(ipfsLinksToCy(data.id, links))
+    runLayout(cy)
   })
 
   return cy
+}
+
+function runLayout (cy) {
+  cy.layout(layoutOpts).run()
 }
